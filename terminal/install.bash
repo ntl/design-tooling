f82a682f327e2151b2f39349ec4dfc3384d7b841
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-19 16:07:53 -0600

    Kitty configuration

--- terminal/install.bash
+++ terminal/install.bash
@@ -16,15 +16,23 @@ installKitty() {
 
   /bin/sh $installer dest=./terminal/kitty launch=n
 
-  mkdir -p ~/bin
+  mkdir -p ~/.local/bin
 
-  ln -svf $PWD/terminal/kitty/kitty.app/bin/kitty ~/bin
+  ln -svf $PWD/terminal/kitty/kitty.app/bin/kitty ~/.local/bin
 }
 
 configureKitty() {
   mkdir -p ~/.config/kitty
 
   ln -svf $PWD/terminal/kitty.conf ~/.config/kitty
+  ln -svf $PWD/terminal/color_scheme.conf ~/.config/kitty
+  ln -svf $PWD/terminal/tab_bar.conf ~/.config/kitty
+  ln -svf $PWD/terminal/typography.conf ~/.config/kitty
+  ln -svf $PWD/terminal/keyboard_shortcuts.conf ~/.config/kitty
+
+  mkdir -p ~/.local/share/applications
+
+  cp -v $PWD/terminal/kitty/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
 }
 
 installKitty
