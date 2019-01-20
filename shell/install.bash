d216f0f9377b9509ddbc6485405883883108c80a
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-20 11:49:52 -0600

    Shell

---
+++ shell/install.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eu
+
+echo
+echo "Configuring Shell"
+echo "= = ="
+echo
+
+configureBash() {
+	mkdir -p ~/.bash
+
+  ln -svf $PWD/shell/bashrc ~/.bashrc
+
+	ln -svf $PWD/shell/completion ~/.bash/completion
+}
+
+configureBash
