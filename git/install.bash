a917dc110cce35b1ab1bf54ea3525de1cff945bb
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-31 11:40:42 -0600

    Git aliases

--- git/install.bash
+++ git/install.bash
@@ -13,11 +13,15 @@ configure() {
   git config --replace-all --global alias.sh show HEAD
   git config --replace-all --global alias.d diff
   git config --replace-all --global alias.dc 'diff --cached'
+  git config --replace-all --global alias.dw 'diff --word-diff=color'
+  git config --replace-all --global alias.dcw 'diff --cached --word-diff=color'
   git config --replace-all --global alias.ca 'commit --amend'
   git config --replace-all --global alias.lg 'log --oneline --decorate --graph'
   git config --replace-all --global alias.r 'rebase --preserve-merges --allow-empty-message'
   git config --replace-all --global alias.ri 'rebase --interactive --preserve-merges --allow-empty-message'
   git config --replace-all --global alias.fb 'filter-branch --tag-name-filter cat'
+  git config --replace-all --global alias.list-remote-tags 'ls-remote --tags origin'
+  git config --replace-all --global alias.empty-commit 'commit --allow-empty --allow-empty-message -m ""'
 }
 
 installBashCompletion() {
