41b0753c7471f7151d437f39f8dfa4924cd85623
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-25 17:01:49 -0500

    InputRC is added

--- shell/install.bash
+++ shell/install.bash
@@ -19,4 +19,9 @@ configureBash() {
 	curl -o $theme_file -z $theme_file https://raw.githubusercontent.com/chriskempson/base16-shell/master/scripts/base16-tomorrow-night.sh
 }
 
+configureInputRC() {
+  ln -svf $PWD/inputrc ~/.inputrc
+}
+
 configureBash
+configureInputRC
