eaec011a065d4cb6ce66d7a1dff1e82588bde62c
Nathan Ladd (Eventide Project) <ntl-evt@ntl-librem-13.attlocal.net>
2019-10-26 09:28:21 -0500

    Kitty is removed

--- terminal/install.bash
+++ terminal/install.bash
@@ -3,39 +3,7 @@
 set -eu
 
 echo
-echo "Installing Terminal Emulator (Kitty)"
+echo "Configuring Terminal Emulator (Tilix)"
 echo "= = ="
 echo
 
-installKitty() {
-  local installer
-
-  installer=$PWD/terminal/kitty/installer.sh
-
-  rm -rf ./terminal/kitty/kitty.app
-
-  curl -o $installer -z $installer 'https://sw.kovidgoyal.net/kitty/installer.sh'
-
-  /bin/sh $installer dest=./terminal/kitty launch=n
-
-  mkdir -p ~/.local/bin
-
-  ln -svf $PWD/terminal/kitty/kitty.app/bin/kitty ~/.local/bin
-}
-
-configureKitty() {
-  mkdir -p ~/.config/kitty
-
-  ln -svf $PWD/terminal/kitty.conf ~/.config/kitty
-  ln -svf $PWD/terminal/color_scheme.conf ~/.config/kitty
-  ln -svf $PWD/terminal/tab_bar.conf ~/.config/kitty
-  ln -svf $PWD/terminal/typography.conf ~/.config/kitty
-  ln -svf $PWD/terminal/keyboard_shortcuts.conf ~/.config/kitty
-
-  mkdir -p ~/.local/share/applications
-
-  cp -v $PWD/terminal/kitty/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
-}
-
-installKitty
-configureKitty
