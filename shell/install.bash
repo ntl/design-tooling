0dc4762fa7ed556980f7251c9a861cd81c593ac0
Nathan Ladd <nathanladd@gmail.com>
2019-10-31 16:36:05 -0500

    Bash profile, not bashrc

--- shell/install.bash
+++ shell/install.bash
@@ -10,7 +10,7 @@ echo
 configureBash() {
 	mkdir -p ~/.bash
 
-  ln -svf $PWD/shell/bashrc ~/.bashrc
+  ln -svf $PWD/shell/bash_profile ~/.bash_profile
 
 	ln -svf $PWD/shell/completion ~/.bash
 
@@ -19,8 +19,8 @@ configureBash() {
 	curl -o ~/.bash/base16-solarized-light.sh https://raw.githubusercontent.com/chriskempson/base16-shell/master/scripts/base16-solarized-light.sh
 	curl -o ~/.bash/base16-solarized-dark.sh https://raw.githubusercontent.com/chriskempson/base16-shell/master/scripts/base16-solarized-dark.sh
 
-  if [ ! -s ~/.bashrc.local ]; then
-    cp -v $PWD/shell/bashrc.local.default ~/.bashrc.local
+  if [ ! -s ~/.bash_profile.local ]; then
+    cp -v $PWD/shell/bash_profile.local.default ~/.bash_profile.local
   fi
 }
 
