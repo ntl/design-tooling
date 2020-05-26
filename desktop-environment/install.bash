f364f884fd6fbe026ed2c017e79881e905d3117c
Nathan Ladd <nathanladd@gmail.com>
2020-05-26 10:59:13 -0500

    Firefox icons

--- desktop-environment/install.bash
+++ desktop-environment/install.bash
@@ -11,6 +11,14 @@ configureGNOME() {
 	dconf load / < $PWD/desktop-environment/gnome.dconf
 }
 
+configureFirefox() {
+  pushd $PWD/desktop-environment/
+
+  curl -o firefox-nightly-assets.zip https://3u26hb1g25wn1xwo8g186fnd-wpengine.netdna-ssl.com/files/2019/10/Firefox-Browser-Nightly-Logo-Assets.zip
+  unzip -u firefox-nightly-assets.zip -d ./firefox-nightly-assets/
+
+  popd
+}
+
 configureGNOME
-
-
+configureFirefox
