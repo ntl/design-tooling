bf06d45efc923e9829281c9656fdddce604c3e40
Nathan Ladd <nathanladd@gmail.com>
2020-06-24 19:15:33 -0500

    GNOME configuration

--- desktop-environment/install.bash
+++ desktop-environment/install.bash
@@ -12,12 +12,16 @@ configureGNOME() {
 }
 
 configureFirefox() {
-  pushd $PWD/desktop-environment/
-
-  curl -o firefox-nightly-assets.zip https://3u26hb1g25wn1xwo8g186fnd-wpengine.netdna-ssl.com/files/2019/10/Firefox-Browser-Nightly-Logo-Assets.zip
-  unzip -u firefox-nightly-assets.zip -d ./firefox-nightly-assets/
-
-  popd
+	pushd $PWD/desktop-environment/
+	
+	curl -o firefox-nightly-assets.zip https://3u26hb1g25wn1xwo8g186fnd-wpengine.netdna-ssl.com/files/2019/10/Firefox-Browser-Nightly-Logo-Assets.zip
+	unzip -u firefox-nightly-assets.zip -d ./firefox-nightly-assets/
+	
+	xdg-icon-resource install --size 512 "firefox-nightly-assets/Icon (Full Color)/Fx-Browser-Nightly-icon-fullColor-512.png" firefox-nightly
+	
+	popd
+	
+	ln -svf $PWD/desktop-environment/firefox-profile-manager.desktop ~/.local/share/applications
 }
 
 configureGNOME
