55859a5bf588d34610badd585deff4b7b35aefc6
Nathan Ladd <nathanladd@gmail.com>
2020-06-26 14:44:39 -0500

    Ruby removed, firefox launcher corrected

--- desktop-environment/install.bash
+++ desktop-environment/install.bash
@@ -11,18 +11,13 @@ configureGNOME() {
 	dconf load / < $PWD/desktop-environment/gnome.dconf
 }
 
-configureFirefox() {
-	pushd $PWD/desktop-environment/
-	
-	curl -o firefox-nightly-assets.zip https://3u26hb1g25wn1xwo8g186fnd-wpengine.netdna-ssl.com/files/2019/10/Firefox-Browser-Nightly-Logo-Assets.zip
-	unzip -u firefox-nightly-assets.zip -d ./firefox-nightly-assets/
-	
-	xdg-icon-resource install --size 512 "firefox-nightly-assets/Icon (Full Color)/Fx-Browser-Nightly-icon-fullColor-512.png" firefox-nightly
-	
-	popd
-	
-	ln -svf $PWD/desktop-environment/firefox-profile-manager.desktop ~/.local/share/applications
+configureLauncher() {
+  desktop-file-validate $PWD/desktop-environment/firefox-user.desktop
+
+  mkdir -p ~/.local/share-applications
+
+  ln -svf $PWD/desktop-environment/firefox-user.desktop ~/.local/share/applications
 }
 
+configureLauncher
 configureGNOME
-configureFirefox
