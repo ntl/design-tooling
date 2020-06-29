7dd268e9da6f9f0a10ac01b970ecc6254573cc2a
Nathan Ladd <nathanladd@gmail.com>
2020-06-29 12:11:06 -0500

    install.bash

--- install.bash
+++ install.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -eu
+
+./packages/install.bash
+
 ./desktop-environment/install.bash
-./terminal/install.bash
-./shell/install.bash
-./editor/install.bash
 ./git/install.bash
-./ruby/install.bash
+./editor/install.bash
 ./projects/install.bash
+./shell/install.bash
+./terminal/install.bash
