420ad6c2c271f7bcf2b5995648c66afc457b03b7
Nathan Ladd <nathanladd@gmail.com>
2020-07-02 15:52:46 -0500

    Ensure PROJECTS_HOME and ARCHIVED_PROJECTS_HOME are set

--- install.bash
+++ install.bash
@@ -2,6 +2,22 @@
 
 set -eu
 
+ready=1
+
+if [ "${PROJECTS_HOME:-}" = "" ]; then
+  echo "ERROR: PROJECTS_HOME is not set"
+  ready=0
+fi
+
+if [ "${ARCHIVED_PROJECTS_HOME:-}" = "" ]; then
+  echo "ERROR: ARCHIVED_PROJECTS_HOME is not set"
+  ready=0
+fi
+
+if [ "$ready" = "0" ]; then
+  exit 1
+fi
+
 ./packages/install.bash
 
 ./desktop-environment/install.bash
